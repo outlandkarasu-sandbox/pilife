dfb12476ca300c886aa17b77cf1a94da104edc5d
outland.karasu <outland.karasu@gmail.com>
2022-02-19 17:31:05 +0900

    issue #5 initialize SDL2.

--- source/app.d
+++ source/app.d
@@ -1,6 +1,37 @@
-import std.stdio;
+/**
+Main module.
+*/
 
+import std.exception :
+    enforce;
+import std.stdio :
+    writefln;
+import std.string :
+    format;
+
+import bindbc.sdl :
+    loadSDL,
+    SDL_INIT_VIDEO,
+    SDL_Init,
+    SDL_Quit,
+    sdlSupport,
+    unloadSDL;
+
+import pilife.sdl :
+    sdlError;
+
+/**
+Main function.
+*/
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+    immutable loadedSDLVersion = loadSDL();
+    enforce(loadedSDLVersion >= sdlSupport, format("loadSDL error: %s", loadedSDLVersion));
+    scope(exit) unloadSDL();
+
+    writefln("loaded SDL: %s", loadedSDLVersion);
+
+    enforce(SDL_Init(SDL_INIT_VIDEO) == 0, sdlError);
+    scope(exit) SDL_Quit();
 }
+
