c6561af517e0ee4076a02fff5fe445058a5275f4
outland.karasu <outland.karasu@gmail.com>
2022-02-19 18:20:04 +0900

    issue #7 add main window.

--- source/app.d
+++ source/app.d
@@ -15,7 +15,23 @@ import bindbc.sdl :
     SDL_Init,
     SDL_Quit,
     sdlSupport,
-    unloadSDL;
+    unloadSDL,
+    SDL_CreateRenderer,
+    SDL_CreateWindow,
+    SDL_Delay,
+    SDL_DestroyRenderer,
+    SDL_DestroyWindow,
+    SDL_SetRenderDrawColor,
+    SDL_RenderClear,
+    SDL_RenderPresent,
+    SDL_Event,
+    SDL_PollEvent,
+    SDL_QUIT,
+    SDL_ShowWindow,
+    SDL_WINDOWPOS_UNDEFINED,
+    SDL_WINDOW_HIDDEN,
+    SDL_RENDERER_ACCELERATED,
+    SDL_RENDERER_PRESENTVSYNC;
 
 import pilife.sdl :
     sdlError;
@@ -32,6 +48,48 @@ void main()
     writefln("loaded SDL: %s", loadedSDLVersion);
 
     enforce(SDL_Init(SDL_INIT_VIDEO) == 0, sdlError);
+
+    auto window = enforce(SDL_CreateWindow(
+        "pilife",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640, 480,
+        SDL_WINDOW_HIDDEN));
+    scope(exit) SDL_DestroyWindow(window);
+
+    auto renderer = enforce(SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC),
+        sdlError);
+    scope(exit) SDL_DestroyRenderer(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_ShowWindow(window);
+
+    mainLoop();
+
     scope(exit) SDL_Quit();
+}
+
+void mainLoop()
+{
+    for (SDL_Event event; ;)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_QUIT:
+                    return;
+                default:
+                    break;
+            }
+        }
+        SDL_Delay(13);
+    }
 }
 
