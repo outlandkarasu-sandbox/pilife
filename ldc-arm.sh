0b522047a40940e043c5edf2563823388f352bf7
outland.karasu <outland.karasu@gmail.com>
2022-02-22 12:40:36 +0900

    issue #11 update for RPi3 build.

--- ldc-arm.sh
+++ ldc-arm.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-ldc2 -mtriple=arm-linux-gnueabihf -mcpu=arm1176jzf-s -gcc=arm-linux-gnueabihf-gcc -L=-L/home/ldc/druntime $@
+ldc2 \
+    -defaultlib=phobos2-ldc,druntime-ldc \
+    -link-defaultlib-shared=false \
+    -mtriple=arm-linux-gnueabihf \
+    -gcc=arm-linux-gnueabihf-gcc \
+    -L=-L/home/ldc/druntime \
+    -L=-L/home/ldc/sysroot/usr/lib \
+    $@
