3925638094885025ad98c42a64111224727ad13d
outland.karasu <outland.karasu@gmail.com>
2022-02-23 10:18:53 +0900

    issue #21 implement properties.

--- source/pilife/game.d
+++ source/pilife/game.d
@@ -2,3 +2,68 @@
 Life game module.
 */
 module pilife.game;
+
+import std.typecons : Nullable;
+
+struct Life
+{
+}
+
+struct Cell
+{
+    uint x;
+    uint y;
+    Nullable!Life life;
+}
+
+struct LifeGame
+{
+    @disable this();
+
+    this(uint width, uint height) @nogc nothrow pure @safe scope
+    {
+        this.width_ = width;
+        this.height_ = height;
+    }
+
+    /**
+    life game world translate to next state.
+    */
+    void next() @nogc nothrow pure @safe
+    {
+    }
+
+    /**
+    For each over world cells.
+    */
+    int opApply(Dg)(scope Dg dg) const
+    {
+        return 0;
+    }
+
+    const @nogc nothrow pure @safe scope
+    {
+        @property uint width()
+        {
+            return width_;
+        }
+
+        @property uint height()
+        {
+            return height_;
+        }
+    }
+
+private:
+    uint width_;
+    uint height_;
+}
+
+///
+@nogc nothrow pure @safe unittest
+{
+    auto game = LifeGame(100, 200);
+    assert(game.width == 100);
+    assert(game.height == 200);
+}
+
