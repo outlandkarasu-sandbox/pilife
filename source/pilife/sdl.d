0f8d38788802c32d99ca8e34d7dc0330923ee068
outland.karasu <outland.karasu@gmail.com>
2022-03-03 09:56:07 +0900

    issue #25 implement get displays.

--- source/pilife/sdl.d
+++ source/pilife/sdl.d
@@ -11,8 +11,11 @@ import std.string :
 import bindbc.sdl :
     SDL_GetDisplayMode,
     SDL_GetError,
+    SDL_GetNumVideoDisplays,
     SDL_GetNumDisplayModes,
-    SDL_DisplayMode;
+    SDL_DisplayMode,
+    SDL_GetDisplayBounds,
+    SDL_Rect;
 
 /**
 SDL error.
@@ -28,5 +31,35 @@ Enforce SDL value.
 auto enforceSDL(T)(auto return scope ref T value)
 {
     return enforce(value, sdlError);
+}
+
+/**
+SDL display information.
+*/
+struct Display
+{
+    SDL_Rect bounds;
+    SDL_DisplayMode[] modes;
+}
+
+/**
+Get SDL displays.
+*/
+Display[] getDisplays()
+{
+    Display[] result;
+    foreach (displayIndex; 0 .. SDL_GetNumVideoDisplays())
+    {
+        Display display;
+        enforceSDL(SDL_GetDisplayBounds(displayIndex, &display.bounds) == 0);
+        foreach (modeIndex; 0 .. SDL_GetNumDisplayModes(displayIndex))
+        {
+            SDL_DisplayMode mode;
+            enforceSDL(SDL_GetDisplayMode(displayIndex, modeIndex, &mode) == 0);
+            display.modes ~= mode;
+        }
+        result ~= display;
+    }
+    return result;
 }
 
