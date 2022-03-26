18c4423fb8e2d917258ca69073888bcecadb4682
outland.karasu <outland.karasu@gmail.com>
2022-03-26 15:12:36 +0900

    issue #41 add OpenGL enforce.

--- source/pilife/opengl.d
+++ source/pilife/opengl.d
@@ -28,7 +28,52 @@ import bindbc.opengl :
     glShaderSource,
     glCompileShader,
     glGetShaderiv,
-    glGetShaderInfoLog;
+    glGetShaderInfoLog,
+    glGetError,
+    GL_NO_ERROR,
+    GL_INVALID_ENUM,
+    GL_INVALID_VALUE,
+    GL_INVALID_OPERATION,
+    GL_INVALID_FRAMEBUFFER_OPERATION,
+    GL_OUT_OF_MEMORY,
+    GL_STACK_UNDERFLOW,
+    GL_STACK_OVERFLOW;
+
+/**
+Enforce OpenGL value.
+*/
+void enforceGL(string file = __FILE__, ulong line = __LINE__)(lazy void dg)
+{
+    dg();
+    immutable error = glGetError();
+    if (error != GL_NO_ERROR)
+    {
+        throw new Exception(glErrorToString(error), file, line);
+    }
+}
+
+string glErrorToString(GLenum error) @nogc nothrow pure @safe
+{
+    switch (error)
+    {
+        case GL_INVALID_ENUM:
+            return "GL_INVALID_ENUM";
+        case GL_INVALID_VALUE:
+            return "GL_INVALID_VALUE";
+        case GL_INVALID_OPERATION:
+            return "GL_INVALID_OPERATION";
+        case GL_INVALID_FRAMEBUFFER_OPERATION:
+            return "GL_INVALID_FRAMEBUFFER_OPERATION";
+        case GL_OUT_OF_MEMORY:
+            return "GL_OUT_OF_MEMORY";
+        case GL_STACK_UNDERFLOW:
+            return "GL_STACK_UNDERFLOW";
+        case GL_STACK_OVERFLOW:
+            return "GL_STACK_OVERFLOW";
+        default:
+            return "UNKNOWN";
+    }
+}
 
 /**
 Compile shaders and create program.
